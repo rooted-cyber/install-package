5e0df58b1948126e47c2efab8f7af09099e4001e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-06-22 03:23:08 +0530

    Update gpt.py

--- plu/gpt.py
+++ plu/gpt.py
@@ -14,6 +14,8 @@
 """
 
 from io import BytesIO
+from asyncio import sleep, TimeoutError
+
 
 from . import async_searcher, LOGS, ultroid_cmd
 
@@ -62,3 +64,21 @@ async def chatgpt2(e):
     except Exception as exc:
         LOGS.exception(exc)
         await eris.edit(f"**Ran into an Error:** \n`{exc}`" )
+
+
+@ultroid_cmd(pattern="msg ?(.*)",manager=True)
+async def msg(event):
+ inp = event.pattern_match.group(1)
+ reply = await event.get_reply_message()
+ if not reply:
+  await event.eor("Reply forward message")
+  return
+ try:
+  await bot.send_message(reply.fwd_from.from_id, f"{inp}")
+  a = reply.fwd_from.from_id.user_id
+  b = await bot.get_entity(a)
+  c = b.username
+  u = (f"@{c}")
+  await event.reply(f"Your message sent {u}")
+ except TimeoutError:
+     await event.eor("Bot not work")
