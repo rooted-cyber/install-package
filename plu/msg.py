a7e37d48a1ed719000d73222e743346d8f4a7e6e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-06-22 03:00:54 +0530

    Add files via upload

--- plu/msg.py
+++ plu/msg.py
@@ -1,10 +1,18 @@
-@ultroid_cmd(pattern="msg ?(.*)")
+from asyncio import sleep, TimeoutError
+
+@ultroid_cmd(pattern="msg ?(.*)",manager=True)
 async def msg(event):
-	inp = event.pattern_match.group(1)
-	reply = await event.get_reply_message()
-	await bot.send_message(reply.fwd_from.from_id, f"{inp}")
-	a = reply.fwd_from.from_id.user_id
-	b = await bot.get_entity(a)
-	c = b.username
-	u = (f"@{c}")
-	await event.reply(f"Your message sent {u}")
+ inp = event.pattern_match.group(1)
+ reply = await event.get_reply_message()
+ if not reply:
+  await event.eor("Reply forward message")
+  return
+ try:
+  await bot.send_message(reply.fwd_from.from_id, f"{inp}")
+  a = reply.fwd_from.from_id.user_id
+  b = await bot.get_entity(a)
+  c = b.username
+  u = (f"@{c}")
+  await event.reply(f"Your message sent {u}")
+ except TimeoutError:
+     await event.eor("Bot not work")
