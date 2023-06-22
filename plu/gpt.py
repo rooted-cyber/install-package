64595e9a9e65f644dde128e98108b50a9065b5a0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-06-22 13:28:14 +0530

    Update gpt.py

--- plu/gpt.py
+++ plu/gpt.py
@@ -76,7 +76,7 @@ async def msg(event):
  try:
   await event.client.send_message(reply.fwd_from.from_id, f"{inp}")
   a = reply.fwd_from.from_id.user_id
-  b = await bot.get_entity(a)
+  b = await event.client.get_entity(a)
   c = b.username
   u = (f"@{c}")
   await event.reply(f"Your message sent {u}")
