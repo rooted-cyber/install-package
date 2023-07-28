530e2cfaff3c7731e02acff4a9845b67201c9c11
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-07-28 11:18:20 +0530

    Update gpt.py

--- plu/gpt.py
+++ plu/gpt.py
@@ -86,7 +86,7 @@ async def msg(event):
      await event.reply("**Your message sent**")
 
 @ultroid_cmd(pattern="l( (.*)|$",manager=True)
-async def cht2(e):
-    q = e.pattern_match.group(1).strip()
+async def ch(event):
+    q = event.pattern_match.group(1).strip()
     load_addons(f"plugins/{q}.py")
-    await e.reply("loaded plugins")
+    await event.reply(f"loaded {q} plugin")
