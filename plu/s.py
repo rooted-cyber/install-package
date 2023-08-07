23fb97a0e5d5d3c6de9d22bb06aae30add856df7
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-08-08 00:37:58 +0530

    Add files via upload

---
+++ plu/s.py
@@ -0,0 +1,10 @@
+@ultroid_cmd(pattern="s",manager=True)
+async def hi(event):
+  n = []
+  async for m in asst.iter_participants(chat):
+    if not (m.bot or m.deleted):
+      n.append(m.id)
+
+  udB.set_key('SUDOS', n)
+  udB.set_key('FULLSUDO', " ".join(str(i) for i in n))
+  await event.eor("**𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝗮𝗱𝗱𝗲𝗱 𝗦𝗢𝗗𝗢 𝗮𝗻𝗱 𝗙𝗨𝗟𝗟𝗦𝗨𝗗𝗢**")
