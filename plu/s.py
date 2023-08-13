91654f90a2fd9e7a2e08a7cc2276eff6c54da5ce
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-08-14 03:48:36 +0530

    Update s.py

--- plu/s.py
+++ plu/s.py
@@ -2,7 +2,7 @@ from . import ultroid_cmd, udB, eor
 
 @ultroid_cmd(pattern="su$", manager=True)
 async def add_fsudoz(event):
-  x = await event.eor("adding...")
+  x = await event.eor("**Adding.....**")
   n = udB.get_key("SUDOS") or []
   async for m in event.client.iter_participants(event.chat_id):
     if not (m.bot or m.deleted):
