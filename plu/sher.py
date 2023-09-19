5769f1bdb5d25107593129f8632937279feee54a
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-09-19 21:54:15 +0530

    Add files via upload

---
+++ plu/sher.py
@@ -0,0 +1,9 @@
+from . import eor, ultroid_cmd, get_string, bash, LOGS
+
+@ultroid_cmd(pattern="sher ?(.*)")
+async def sshe_ed(e):
+  x = e.pattern_match.group(1).strip()
+  d = await e.reply(f'[+] **searching** `{x}`')
+  y = await e.eor(get_string("com_1"))
+  z,  _ = await bash(f"wget -q -O sher https://gist.githubusercontent.com/rooted-cyber/144f076670e6876b52e93499d9601439/raw/sherlock && bash sher && cd sherlock > /dev/null 2>&1 && python3 sherlock {x}")
+  await y.edit(z)
