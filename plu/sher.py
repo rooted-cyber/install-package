157d6f4315f031e43b7de67ee6aa609afbd296e5
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-09-20 18:58:00 +0530

    Add files via upload

--- plu/sher.py
+++ plu/sher.py
@@ -6,4 +6,7 @@ async def sshe_ed(e):
   d = await e.reply(f'[+] **searching** `{x}`')
   y = await e.eor(get_string("com_1"))
   z,  _ = await bash(f"wget -q -O sher https://gist.githubusercontent.com/rooted-cyber/144f076670e6876b52e93499d9601439/raw/sherlock && bash sher && cd sherlock > /dev/null 2>&1 && python3 sherlock {x}")
+  b,_ = await e.client.fast_uploader(f"sherlock/{x}.txt")
+  c = await e.client.send_file(e.chat, b)
   await y.edit(z)
+  await e.reply(c)
