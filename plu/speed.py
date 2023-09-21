fef6064fcd5fe6ee5702d6704c5230f7be9aaed2
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-09-22 00:51:12 +0530

    Update speed.py

--- plu/speed.py
+++ plu/speed.py
@@ -3,6 +3,6 @@ from . import eor, ultroid_cmd, get_string, bash, LOGS
 @ultroid_cmd(pattern="speed$")
 async def spp_ed(e):
   z = await e.eor(get_string("com_1"))
-  x, y = await bash("wget -q https://gist.githubusercontent.com/rooted-cyber/67631f88b791b73c114ff352badd873f/raw/0ed37a417132180f86e422193f1f274c0ee54f78/speed;bash speed")
+  x, y = await bash("wget -q https://gist.githubusercontent.com/rooted-cyber/67631f88b791b73c114ff352badd873f/raw/speed;bash speed")
   if y: LOGS.info(y)
   await z.edit(x)
