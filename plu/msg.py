2144e5a8fb31b100b3170d1c5baf05bd605047e4
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-10-12 16:51:37 +0530

    Add files via upload

---
+++ plu/msg.py
@@ -0,0 +1,7 @@
+from . import ultroid_cmd, get_string
+@ultroid_cmd(pattern="msg$")
+async def hi(e):
+  reply = await e.get_reply_message()
+  a = e.input_chat.channel_id
+  b = reply.id
+  await e.respond(f"msg link : tg://privatepost?channel={a}&amp&post={b} \nor\n https://t.me/c/{a}/{b}")
