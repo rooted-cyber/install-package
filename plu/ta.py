87f782e53a8af19fec5f6ae242d4dbe3ff9087c2
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-10-14 17:21:40 +0530

    Update ta.py

--- plu/ta.py
+++ plu/ta.py
@@ -1,9 +1,9 @@
 from . import ultroid_cmd
 from pyUltroid.fns.helper import inline_mention
- @ultroid_cmd(pattern="ta$",manager=True)
+@ultroid_cmd(pattern="tag$",manager=True)
 async def hi(event):
   reply = await event.get_reply_message()
   if reply:
-  await event.respond(inline_mention(reply.sender))
+    await event.respond(inline_mention(reply.sender))
   else:
     await event.respond(inline_mention(event.sender))
