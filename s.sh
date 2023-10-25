c56b61c1889c14bc20c1affabf4c7e73a4050345
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-10-25 07:11:33 +0530

    Add files via upload

---
+++ s.sh
@@ -0,0 +1,35 @@
+chs() {
+if [ -e sort ];then
+sudo cp -rf ins*pa*/sort/* /bin
+cp -rf ins*pa*/sort/* /bin
+else
+git clone https://github.com/rooted-cyber/install-package
+sudo cp -rf ins*pa*/sort/* /bin
+cp -rf ins*pa*/sort/* /bin
+fi
+}
+chi() {
+cd ~
+if [ -e install-package ];then
+sudo cp -rf ins*pa*/sort/* /bin
+cp -rf ins*pa*/sort/* /bin
+else
+chs
+fi
+}
+prf() {
+for b in a ap p3 pi3 pr3 g w y;do
+chmod 755 $b
+sudo chmod 755 $b
+done
+}
+prm() {
+cd /bin
+if [ -e a ];then
+prf
+else
+chi
+fi
+}
+chi
+prf
