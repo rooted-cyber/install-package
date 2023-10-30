108281b2afd70e843edd6f89992e87cf424c1702
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-10-30 10:29:35 +0530

    Add files via upload

--- plu/load.py
+++ plu/load.py
@@ -5,14 +5,14 @@ from . import ultroid_cmd, get_string
 @ultroid_cmd(pattern="l( (.*)|$)",manager=True)
 async def ch(e):
   q = e.pattern_match.group(1).strip()
-  reply = await event.get_reply_message()
+  reply = await e.get_reply_message()
   if reply:
     await e.respond("**Type plugin name not reply**")
     return
   b = f"{q}.py"
   d = ls("plugins")
   if b in d:
-    await event.eor(get_string("com_1"))
+    await e.eor(get_string("com_1"))
     load_addons(f"plugins/{q}.py")
     await e.reply(f"Successfully loaded **{q}**")
   else:
