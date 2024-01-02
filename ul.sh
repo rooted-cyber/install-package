37a3052c8a1c85874811ca2bea20ad6d0bdeccfb
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-01-02 10:19:24 +0530

    Add files via upload

---
+++ ul.sh
@@ -0,0 +1,83 @@
+pentt() {
+echo "️️"
+echo "️️"
+echo "️️"
+echo "️️"
+}
+centt() {
+echo "️️"
+echo "️️"
+echo "️️"
+echo "️️"
+}
+fix() {
+apt install --fix-broken -y
+}
+nfc() {
+neofetch --stdout
+cd ~
+if [ -e .config ];then
+cp ~/install*e/config* ~/.config/neofetch
+fi
+}
+chcp() {
+cd ~/install*e/plu
+if [ -e pwd.py ];then
+centt
+printf "Founding all plugins"
+centt
+printf "Now copying all plugins"
+centt
+cp *py ~/install*e/Ultroid/plug*
+ls ~/install*e/Ultroid/plug*
+sleep 50
+fi
+}
+pplf() {
+centt
+printf "Copying file or plugins"
+centt
+cd ~/install*e
+if [ -e a.otf ];then
+centt
+printf "Founding all files"
+centt
+printf "Now copying"
+centt
+nfc
+cp -rf * ~/install*e/Ultroid/res*/dow*
+ls ~/install*e/Ultroid/res*/dow*
+sleep 50
+centt
+cd ~/install*e/plu
+chcp
+else
+centt
+printf "Not found"
+exit
+fi
+}
+app() {
+apt update
+apt upgrade -y
+fix
+for a in wget python3-pip python3 ffmpeg neofetch mediainfo;do
+pentt
+printf "Installing $a"
+echo
+apt install  $a -y
+pentt
+fix
+done
+pip install --upgrade pip
+for bb in yt-dlp telegraph aiohttp python-decouple requests;do
+pentt
+printf "Installing $a"
+echo
+pip3 uninstall $bb -y
+pip3 install $bb
+pentt
+done
+}
+app
+pplf
