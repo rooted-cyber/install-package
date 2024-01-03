44c30a373fd37a8825935f6542616f6a1e0fbba2
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-01-03 08:01:25 +0530

    Add files via upload

--- ul.sh
+++ ul.sh
@@ -30,34 +30,12 @@ printf "Now copying all plugins"
 centt
 cp *py ~/install*e/Ultroid/plug*
 ls ~/install*e/Ultroid/plug*
-sleep 50
 fi
 }
 pplf() {
-centt
-printf "Copying file or plugins"
-centt
 cd ~/install*e
 if [ -e a.otf ];then
-centt
-printf "Founding all files"
-centt
-printf "Now copying"
-centt
-cp -rf a* b* p* n* c* /root/install-package/Ultroid/resources/downloads
-ls /root/install-package/Ultroid/resources/downloadsnfc
-echo "hrllo,"
-cd ~/install*e
-pwd
-centt
-echo "ls"
-cp -rf * ~/ins*e/Ultroid/res*/dow*
-ls ~/ins*l*e/Ultroid/res*/dow*
-exit
-crntt
-ls ~/inst*
-neofe
-sleep 50
+nfc
 centt
 cd ~/install*e/plu
 chcp
@@ -73,17 +51,10 @@ apt upgrade -y
 fix
 for a in wget python3-pip python3 ffmpeg neofetch mediainfo;do
 pentt
-cd /bin
-if [ -e $a ];then
-echo "already"
-else
-printf "ajib"
-printf "Installing $a"
+printf "package Installing $a"
 echo
 apt install  $a -y
 pentt
-sleep 5
-fi
 fix
 done
 pip install --upgrade pip
