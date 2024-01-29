92e93daa0c9497e497bb6fbf9b18043485c93370
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-01-29 14:29:38 +0530

    Add files via upload

--- plu/mysudo.py
+++ plu/mysudo.py
@@ -1,4 +1,4 @@
-from . import eor, ultroid_cmd, get_string, eor, bash, inline_mention, LOGS, udB
+from . import eor, SUDO_HNDLR, ultroid_cmd, get_string, eor, bash, inline_mention, LOGS, udB
 
 @ultroid_cmd(pattern="ssuu")
 async def szudo(e):
@@ -7,6 +7,15 @@ async def szudo(e):
   udB.set_key("SUDOS",rid)
   udB.set_key("FULLSUDO",rid)
   name = await e.client.get_entity(int(rid))
+  una = name.username
+  fn = name.first_name
+  ln = name.last_name
   men = inline_mention(name)
   ii = udB.get_key("FULLSUDO")
-  await e.reply(f"**Added** {men} **as FULLSUDO and SUDO User** and \n id : {ii}")
+  await e.reply(f"""**Added** {men} **as FULLSUDO and SUDO User**
+First name : {fn}
+Last name : {ln}
+id : {ii}
+username : {una}
+SUDO_HNDLR : {SUDO_HNDLR}
+""")
