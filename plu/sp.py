e0a2cec9d8f4e2d61ef9c03c7d8a32e1c9c58972
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-06-04 02:26:11 +0530

    Update sp.py

--- plu/sp.py
+++ plu/sp.py
@@ -5,9 +5,5 @@ from os import chdir as cd, chmod as ch, listdir as ls
 async def spp_ed(e):
   z = await e.eor(get_string("com_1"))
   x, y = await bash("wget -O speed https://gist.githubusercontent.com/rooted-cyber/67631f88b791b73c114ff352badd873f/raw/speed;bash speed | grep -e 'Test' -e 'Down' -e 'Upload'")
-  if "speed" in ls("/data/data/com.termux/files/usr/bin"):
-    await e.reply("available")
-  else:
-    await bash("cp speed $PREFIX/bin;chmod 777 $PREFIX/bin/speed")
   if y: LOGS.info(y)
   await z.edit(f"`{x}`")
