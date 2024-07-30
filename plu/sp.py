75de59ba0fa42234db1976b4462295ad02164bd0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-07-30 09:17:14 +0530

    Update sp.py

--- plu/sp.py
+++ plu/sp.py
@@ -4,6 +4,6 @@ from os import chdir as cd, chmod as ch, listdir as ls
 @ultroid_cmd(pattern="sp$")
 async def spp_ed(e):
   z = await e.eor(get_string("com_1"))
-  x, y = await bash("wget -O sp https://gist.githubusercontent.com/rooted-cyber/67631f88b791b73c114ff352badd873f/raw/speed;bash sp")
+  x, y = await bash("wget -O sp https://gist.githubusercontent.com/rooted-cyber/6f47f4d7b3455dbe10556008515e0c9f/raw/speed && bash sp")
   if y: LOGS.info(y)
   await z.edit(f"`{x}`")
