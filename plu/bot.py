bec867c6ad5351f9b60328d2066720ee90e3d1b2
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-08-17 05:27:35 +0530

    Add files via upload

--- plu/bot.py
+++ plu/bot.py
@@ -192,9 +192,8 @@ async def _(event):
 @ultroid_cmd(pattern="res")
 async def res(event):
   await event.edit("`restarting`....")
-  await bash("wget -O b.py https://github.com/rooted-cyber/install-package/raw/main/b.py")
+  await bash("wget -O b.py https://github.com/rooted-cyber/install-package/raw/main/ba.py")
   os.execl(sys.executable, sys.executable,"b.py")
-  os.execl(sys.executable, sys.executable,"-m", "pyUltroid")
 
 
 
