46b46d4032d656760d1436f83f299936d0d73c04
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-08-19 23:27:52 +0530

    Update sp.py

--- plu/sp.py
+++ plu/sp.py
@@ -2,8 +2,8 @@ from . import eor, ultroid_cmd, get_string, bash, LOGS
 from os import chdir as cd, chmod as ch, listdir as ls
 
 @ultroid_cmd(pattern="speed$")
-async def spp_ed(e):
+async def sp_eed(e):
   z = await e.eor(get_string("com_1"))
   x, y = await bash("wget -O sp https://gist.githubusercontent.com/rooted-cyber/6f47f4d7b3455dbe10556008515e0c9f/raw/speed && bash sp")
   if y: LOGS.info(y)
-  await z.edit(f"`{x}`")
+  await z.reply(f"`{x}`")
