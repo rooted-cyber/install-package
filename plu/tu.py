17045e3a4a32add56e39862f0a94db29f842f80c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-08-20 14:03:08 +0530

    Update tu.py

--- plu/tu.py
+++ plu/tu.py
@@ -5,8 +5,9 @@ async def cht(e):
   a = e.pattern_match.group(1).strip()
   await e.eor(get_string("com_1"))
   if not a:
-    a = await bash("tu")
-    await e.reply(a)
+    c,b = await bash(f"tu")
+p(b))
+    await e.reply(b)
   else:
     a,b = await bash(f"tu {a}")
     await e.reply(a)
