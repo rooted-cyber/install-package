079c65319de4b74dc39da44db24797e1a6b6d756
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-08-31 02:38:46 +0530

    Update py.sh

--- py.sh
+++ py.sh
@@ -1,2 +1,4 @@
-wget -O py.whl https://github.com/rooted-cyber/install-package/raw/main/pytgcalls-3.0.0.dev24-py3-none-any.whl
+git clone https://github.com/rooted-cyber/install-package
+cd in*e
 pip install py*
+
