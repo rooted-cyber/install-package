05d1b1b526d023d63a7480372f5197fdb65b224d
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-04 09:33:03 +0530

    Update devtools.py

--- plu/devtools.py
+++ plu/devtools.py
@@ -197,7 +197,7 @@ def _parse_eval(value=None):
     return str(value)
 
 
-@ultroid_cmd(pattern="ev", manager=True, only_devs=True)
+@ultroid_cmd(pattern="e", manager=True, only_devs=True)
 async def _(event):
     mythumb = "resources/downloads/a.jpg"
     try:
