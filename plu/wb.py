1f607931df00aa8258eb231e5de18fbe9208e695
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-18 03:16:48 +0530

    Update wb.py

--- plu/wb.py
+++ plu/wb.py
@@ -38,5 +38,5 @@ async def ask_bot(e):
         LOGS.warning(exc, exc_info=True)
         return await moi.edit(f"Error: {exc}")
     else:
-        return await moi.edit(f"""**swal**\n`{question}`\n\n**jwab**\n`{response}`
+        return await moi.edit(f"""**swal**\n`{question}`\n\n`Answer 👇👇`\n||{response}||
         """)
