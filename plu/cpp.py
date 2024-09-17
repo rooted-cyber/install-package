3b05d8780fdcb498974678ef4e2bf1bee8e8ee7c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-17 16:00:38 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -2,6 +2,6 @@ from . import ultroid_cmd, get_string, bash
 
 @ultroid_cmd(pattern="cpp",manager=True)
 async def cht(e):
-  await e.eor(get_string("com_1"))
+  c = await e.eor(get_string("com_1"))
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
-  await e.reply(a)
+  await c.reply(a)
