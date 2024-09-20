7ea14659758bd7fa92b46b7ef63ab95fce924fd7
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-21 01:07:53 +0530

    Update sm.py

--- plu/sm.py
+++ plu/sm.py
@@ -5,9 +5,9 @@ async def chht(e):
   a = e.pattern_match.group(1).strip()
   reply = await e.get_reply_message()
   if not a:
-        if reply and reply.text:
+    if reply and reply.text:
             a = reply.message
-    if not a:
+  if not a:
       return await e.eor("Kuch likho")
       
   await e.eor(get_string("com_1"))
