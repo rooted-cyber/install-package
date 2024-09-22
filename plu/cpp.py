08eb77f66da3a68504aeb4285606365599535c99
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-22 21:06:27 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -5,14 +5,4 @@ async def cht(e):
   c = await e.eor(get_string("com_1"))
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
   await c.reply(a)
-  ok = await ult.eor(get_string("bot_5"))
-  call_back()
-  who = "bot" if ult.client._bot else "user"
-  udB.set_key("_RESTART", f"{who}_{ult.chat_id}_{ok.id}")
-    
-  if len(sys.argv) > 1:
-    os.execl(sys.executable, sys.executable, "main.py")
-  else:
-    os.execl(sys.executable, sys.executable, "-m", "pyUltroid")
-
-
+  
