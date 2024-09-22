b1ee46b6bc9ef316373a8e62b675d3045c1e214a
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-22 14:20:26 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -1,7 +1,14 @@
 from . import ultroid_cmd, get_string, bash
+import os, sys
 
 @ultroid_cmd(pattern="cpp",manager=True)
 async def cht(e):
   c = await e.eor(get_string("com_1"))
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
   await c.reply(a)
+  if len(sys.argv) > 1:
+    os.execl(sys.executable, sys.executable, "main.py")
+  else:
+    os.execl(sys.executable, sys.executable, "-m", "pyUltroid")
+
+
