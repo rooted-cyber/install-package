36f0b34af2662966144245c3bc9bf302e800c8db
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-26 09:59:56 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -4,5 +4,5 @@ import os, sys
 async def cht(e):
   c = await e.eor(get_string("com_1"))
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
-  await c.reply(a)
+  await c.edit(a)
   
