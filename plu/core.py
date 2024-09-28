6ab04b240e9046b269d3561773f5e26c5f4fa4bd
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-29 05:20:47 +0530

    Update core.py

--- plu/core.py
+++ plu/core.py
@@ -4,7 +4,10 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
+try:
+    from pyUltroid._my.my import *
+except:
+    leave_group = None
 
 from . import get_help
 
