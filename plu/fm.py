00da60aaedf419bccc98091d07fc165dfaeb3371
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-01 21:04:02 +0530

    Update fm.py

--- plu/fm.py
+++ plu/fm.py
@@ -15,7 +15,7 @@ async def hi(event):
   l = f"{a.last_name}"
   u = f"{a.username}"
   ph = f"{a.phone}"
-  await event.respond(f"First Name: `{b}`\nLast Name: `{l}`\nUsername: `@{u}`\nPhone: `+{ph}`")
+  await event.respond(file=pic,f"First Name: `{b}`\nLast Name: `{l}`\nUsername: `@{u}`\nPhone: `+{ph}`")
 
 
 @ultroid_cmd(pattern="fm( (.*)|$)",manager=True)
