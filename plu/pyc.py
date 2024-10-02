1e728edc16702d1f6abef0d1882a353b2863a7d3
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-03 03:28:52 +0530

    Update pyc.py

--- plu/pyc.py
+++ plu/pyc.py
@@ -1,9 +1,9 @@
 from pyUltroid.startup.loader import load_addons
 from os import listdir as ls
-from . import ultroid_cmd, get_string
+from . import ultroid_cmd, get_string, udB
 
 @ultroid_cmd(pattern="pyc( (.*)|$)",manager=True)
-async def ch(e):
+async def pyc(e):
   q = e.pattern_match.group(1).strip()
   reply = await e.get_reply_message()
   if reply:
