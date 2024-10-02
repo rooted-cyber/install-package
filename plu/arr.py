cfb9c17cdb17479a6f8de41bc1b3f333770345c9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-03 03:58:12 +0530

    Delete plu/arr.py

--- plu/arr.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-from telethon import events, types
-from . import ultroid_bot
-
-emojis = ("😍","🔥","❤️","🤔","👍","😁","🥰")
-
-@ultroid_bot.on(events.NewMessage(func=lambda e: e.out or (e.mentioned and not e.is_private)))
-async def rootedcyber(rootedcyber):
-  await rootedcyber.react([types.ReactionEmoji(choice(emojis))], big=False if rootedcyber.out else choice((True, False)))
