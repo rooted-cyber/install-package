e3ec09d8a2ac98d9cedb321dbeb676e7536b794e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-03 02:43:06 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -5,6 +5,5 @@ from pyUltroid._my.my import *
 async def cht(e):
   c = await e.eor(get_string("com_1"))
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
-  p = file=await photo(e)
-  await c.edit(f"{a}\n\n{p}")
+  await c.edit(f"{a}\n\n")
   
