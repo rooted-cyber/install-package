0a281da06003a55b5d3e0c4ef5e34f54457b3752
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-07 23:03:00 +0530

    Update sher.py

--- plu/sher.py
+++ plu/sher.py
@@ -4,13 +4,9 @@ from io import BytesIO as open
 async def sshe_ed(e):
   x = e.pattern_match.group(1).strip()
   d = await e.edit(f'[+] **searching** `{x}`')
-  z,  _ = await bash("sher")
+  await bash("sher")
   a = await bash(f"sher {x}")
   if len(a) > 4096:
-    with open(a.encode()) as sh:
-      sh.name = "details.txt"
-      await e.respond(file=sh)
-  #b,_ = await e.client.fast_uploader(f"{x}.txt")
-  #c = await e.client.send_file(e.chat, b)
+    await e.reply("Not poslsible")
   await d.edit(a)
   
