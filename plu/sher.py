648923bdf5ebf710a60aea5376996398c065e6f0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-07 21:58:48 +0530

    Update sher.py

--- plu/sher.py
+++ plu/sher.py
@@ -5,9 +5,13 @@ async def sshe_ed(e):
   x = e.pattern_match.group(1).strip()
   d = await e.reply(f'[+] **searching** `{x}`')
   y = await e.eor(get_string("com_1"))
-  z,  _ = await bash(f"wget -q -O sher https://gist.githubusercontent.com/rooted-cyber/aa5c3f2e24c37be15c5e40157b09873f/raw/97e61757789bf1850302041629298be91ae3309e/sher && bash sher > /dev/null 2>&1")
+  z,  _ = await bash(f"command -v sherlock || sher;wget -q -O sher https://gist.githubusercontent.com/rooted-cyber/aa5c3f2e24c37be15c5e40157b09873f/raw/97e61757789bf1850302041629298be91ae3309e/sher && bash sher > /dev/null 2>&1")
   a = await bash("sherlock {x}")
+  if len(a) > 4096:
+    with BYtesI9(a.encode()) as sh:
+      sh.name = "details.txt"
+      await e.respond(file=sh)
   b,_ = await e.client.fast_uploader(f"{x}.txt")
   c = await e.client.send_file(e.chat, b)
-  await y.reply(z)
+  await y.reply(a)
   
