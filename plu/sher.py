5caa5ac5d7005610876cf5a2b1116b23c5d15b8f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-07 23:14:09 +0530

    Update sher.py

--- plu/sher.py
+++ plu/sher.py
@@ -1,11 +1,11 @@
 from . import eor, ultroid_cmd, get_string, bash, LOGS
 from io import BytesIO as open
+from os import system as s
 @ultroid_cmd(pattern="sher ?(.*)")
 async def sshe_ed(e):
   x = e.pattern_match.group(1).strip()
   d = await e.edit(f'[+] **searching** `{x}`')
-  await bash("sher")
-  a = await bash(f"sher {x}")
+  a = s(f"sher;sher {x}")
   if len(a) > 4096:
     await e.reply("Not poslsible")
   await d.edit(a)
