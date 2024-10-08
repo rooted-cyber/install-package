298e76fdf31142afb10377e8c304cc06f836afcb
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-08 19:43:50 +0530

    Update bot.py

--- plu/bot.py
+++ plu/bot.py
@@ -192,6 +192,7 @@ async def _(event):
     end = round((time.time() - start) * 1000)
     reply_to_id = event.reply_to_msg_id or event.id
     uptime = time_formatter((time.time() - start_time) * 1000)
+    await event.delete()
     x = await event.respond(f"**Bot start time** : `{uptime}`\n**Time** : `{b}`\n**Date** : {a}",file=udB.get_key("ALIVE_PIC"),parse_mode="md",reply_to=reply_to_id)
     
     
