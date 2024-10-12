27ffec0cfd284b3fe26106cfe60abaf3c38785d8
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-12 10:17:48 +0530

    Add files via upload

---
+++ plc/renam-f.py
@@ -0,0 +1,15 @@
+import asyncio
+@ultroid_cmd(pattern="file ?(.*)")
+async def hi(event):
+  text = event.pattern_match.group(1)
+  ab = await event.get_reply_message()
+  if not ab:
+    await event.edit("`Reply any file or type name !!`")
+    return
+  await event.edit("`processing..`")
+  await ab.download_media(text)
+  await event.edit("`Proccessing Done` ✔️")
+  await asyncio.sleep(1)
+  await event.edit("`Now sending file`")
+  await asyncio.sleep(0.1)
+  await bot.send_file(event.chat_id, text,force_document=True)
---
+++ plc/share.py
@@ -0,0 +1,9 @@
+from telethon import events
+@ultroid.on(events.NewMessage(func=lambda c:  c.text and c.media, incoming=False))
+async def linkss(e):
+  if m := getattr(e.media, "webpage", None):
+    doc = m.photo if m.photo else m.document
+    try:
+      await e.respond(m.description, file=doc)
+    except Exception as ex:
+      return LOGS.exception(ex)
