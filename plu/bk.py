acd5eb1a608899f4fe09dced45e17a2eaa383ce0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-13 13:37:42 +0530

    Update bk.py

--- plu/bk.py
+++ plu/bk.py
@@ -2,10 +2,10 @@ from . import ultroid_cmd, get_string, udB
 
 @ultroid_cmd(pattern="gd",manager=True)
 async def gd(e):
-  udB.set("GDRIVE_FOLDER_ID","1wGhvUWjslNUZ38bJoHAzIsLZRuRbi4fj")
-  udB.set("GDRIVE_CLIENT_ID","69689902615-3b0c0tgg7me2pulu9vftvnsf9o9mpf6i.apps.googleusercontent.com")
-  udB.set("GDRIVE_CLIENT_SECRET","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")
-  udB.set("SUDO_HNDLR","$")
-  udB.set("HNDLR","$")
-  udB.set("ADDONS", True)
+  udB.set_key("GDRIVE_FOLDER_ID","1wGhvUWjslNUZ38bJoHAzIsLZRuRbi4fj")
+  udB.set_key("GDRIVE_CLIENT_ID","69689902615-3b0c0tgg7me2pulu9vftvnsf9o9mpf6i.apps.googleusercontent.com")
+  udB.set_key("GDRIVE_CLIENT_SECRET","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")
+  udB.set_key("SUDO_HNDLR","$")
+  udB.set_key("HNDLR","$")
+  udB.set_key("ADDONS", True)
   await e.reply("successfully set")
