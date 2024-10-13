3736d9f2475de8f65f5987e6300e62d48b9d3353
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-13 15:35:19 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -4,6 +4,5 @@ import os, sys
 async def cht(e):
   c = await e.eor(get_string("com_1"))
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
-  d,e = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/edc0ac4c7da73e933ad954f90c40447e/raw/py | bash")
-  await c.edit(f"{a}\n\n {d}")
+  await c.edit(f"{a}\n\n")
   
