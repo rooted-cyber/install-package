ea72045b5d3680ff108897a3d9294f306482995e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-13 06:32:24 +0530

    Delete plu directory

--- plu/py.py
+++
@@ -1,8 +0,0 @@
-from . import ultroid_cmd, get_string, bash, callback
-import os, sys
-@ultroid_cmd(pattern="py$",manager=True)
-async def cht(e):
-  c = await e.eor(get_string("com_1"))
-  a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/edc0ac4c7da73e933ad954f90c40447e/raw/py | bash")
-  await c.edit(a)
-  
