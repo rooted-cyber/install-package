8c0da4d91e03fd84e48b590bb2691962c0655b3e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-18 22:16:30 +0530

    Update akenoai.py

--- plu/akenoai.py
+++ plu/akenoai.py
@@ -1,8 +1,9 @@
-from . import ultroid_cmd
+from . import *
 @ultroid_cmd(pattern="akn$",manager=True)
 async def hi(event):
   reply = await event.get_reply_message()
   import akenoai as dev
+  await event.eor(get_string("com_1"))
   api = dev.AkenoPlus(key=...)
   response = await api.chatgpt_old(query=reply.text)
   data = await api.get_json(response=response)
