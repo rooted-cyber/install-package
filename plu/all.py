1f6e23f55982cfe25369dde521ae0e92247189ed
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-18 06:37:38 +0530

    Create all.py

---
+++ plu/all.py
@@ -0,0 +1,12 @@
+from pyUltroid._my.my import *
+from . import eor, ultroid_cmd
+from os import getcwd as pwd
+
+@ultroid_cmd(pattern="all")
+async def sshe_ed(e):
+  r = 
+  if r:
+    await dl(e)
+    await photo(e)
+  else:
+    await photo(e)
