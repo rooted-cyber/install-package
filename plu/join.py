379845f86726536a109dedda07ab89ae5f6d4baa
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-18 16:08:38 +0530

    Update join.py

--- plu/join.py
+++ plu/join.py
@@ -5,15 +5,10 @@ from . import *
 @ultroid_cmd(pattern="j( (.*)")
 async def join_channel_or_group(event):
     input_str = event.pattern_match.group(1)
-
+    li = input_str
     try:
-        # Check if the input is a complete invite link or just a username
-        if input_str.startswith('https://t.me/'):
-            # Extract the invite hash or username
-            input_str = input_str.split('/')[-1]
-
-        await bot(JoinChannelRequest(input_str))
-        await event.respond(f"**Successfully joined** {input_str}")
+        await bot(JoinChannelRequest(f"{li}"))
+        await event.respond(f"**Successfully joined** \n {input_str}")
     except UserAlreadyParticipantError:
         await event.respond(f"You're already a member of {input_str}.")
     except Exception as e:
