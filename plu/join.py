87ebbe02c4e929e79e3f2bbb4c30f567d74d0500
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-19 11:34:27 +0530

    Update join.py

--- plu/join.py
+++ plu/join.py
@@ -6,10 +6,5 @@ from . import *
 async def join_channel_or_group(event):
     input_str = event.pattern_match.group(1)
     li = input_str
-    try:
-        await bot(JoinChannelRequest(f"{li}"))
-        await event.respond(f"**Successfully joined** \n {input_str}")
-    except UserAlreadyParticipantError:
-        await event.respond(f"You're already a member of {input_str}.")
-    except Exception as e:
-        await event.respond(f"Failed to join {input_str}. Error: {str(e)}")
+    await bot(JoinChannelRequest(f"{li}"))
+    await event.respond(f"**Successfully joined** \n {input_str}")
