5bb1e111c16c4000ece47c205a8e38c7f148baed
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-20 02:42:39 +0530

    Add files via upload

---
+++ plu/op.py
@@ -0,0 +1,27 @@
+from . import ultroid_cmd, get_string, get_paste
+@ultroid_cmd(
+    pattern="op( (.*)|$)",
+)
+async def _(event):
+    a = await event.get_reply_message()
+    b = event.pattern_match.group(1).strip()
+    if not ((a and a.media) or (b and os.path.exists(b))):
+        return await event.eor(get_string("cvt_7"), time=5)
+    xx = await event.eor(get_string("com_1"))
+    rem = None
+    if not b:
+        b = await a.download_media()
+        rem = True
+    try:
+        with open(b) as c:
+            d = c.read()
+    except UnicodeDecodeError:
+        return await xx.eor(get_string("cvt_8"), time=5)
+    try:
+        await xx.edit(f"```{d}```")
+    except BaseException:
+        what, key = await get_paste(d)
+        await xx.edit(f"**MESSAGE EXCEEDS TELEGRAM LIMITS**)
+       await event.client.inline_query(asst.me.username"pasta-{key}") 
+    if rem:
+        os.remove(b)
