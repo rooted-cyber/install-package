55335a13fdb5ac62bc5ac3d08fe6c848ef36bc9a
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-20 05:28:50 +0530

    Update join.py

--- plu/join.py
+++ plu/join.py
@@ -2,7 +2,7 @@ from telethon import events, TelegramClient
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
 from . import *
-@ultroid_cmd(pattern="j( (.*)")
+@ultroid_cmd(pattern="j( (.*)|$)")
 async def join_channel_or_group(event):
     input_str = event.pattern_match.group(1)
     li = input_str
