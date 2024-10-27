f6578c3112645dc593a7e435e9d1c3562d770d09
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-27 11:56:26 +0530

    Update fm.py

--- plu/fm.py
+++ plu/fm.py
@@ -1,19 +1,5 @@
 from telethon import events
-from . import ultroid_cmd
 from pyUltroid._my.my import *
 
 @ultroid_cmd(pattern="fm( (.*)|$)",manager=True)
 async def hi(e):
-  reply = await e.get_reply_message()
-  ty = e.pattern_match.group(1).strip()
-  if not reply:
-    return await e.eor("not reply...", time=5)
-  a = reply.sender  # await event.client.get_entity(rep)
-  b = a.first_name
-  l = a.last_name or ""
-  u = ("@" + a.username) if a.username else "???"
-  ph = a.phone
-  fr = "First_name: ", "`",a.first_name,"`"
-  las = "Last Name: ", "`",a.last_name,"`"
-  pic = await photo(e)
-  await reply.respond(f"\nFirst Name: `{b}`\nLast Name: `{l}`\nUsername: `{u}`\nPhone: `+{ph}`\n\n{fr}\n{las}",file=pic)
