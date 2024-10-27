330f30f1871dfa54608337cac95498f10c9ec44f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-27 11:59:52 +0530

    Update ar.py

--- plu/ar.py
+++ plu/ar.py
@@ -7,3 +7,13 @@ async def linkrss(e):
 
   a = "😍","🔥","❤️","🤔","👍","😁","🥰"
   await e.react([types.ReactionEmoji(f"{c(a)}")])
+
+from random import choice
+from telethon import events, types
+from . import ultroid_bot
+
+emojis = ("😍","🔥","❤️","🤔","👍","😁","🥰")
+
+@ultroid_bot.on(events.NewMessage(func=lambda e: e.out or (e.mentioned and not e.is_private)))
+async def rootedcyber(rootedcyber):
+  await rootedcyber.react([types.ReactionEmoji(choice(emojis))], big=False if rootedcyber.out else choice((True, False)))
