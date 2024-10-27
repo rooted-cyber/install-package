1c9832cb041d4f5ce8ab4663daf064403b5ffdcf
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-27 12:09:43 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -6,3 +6,11 @@ async def cht(e):
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
   await c.edit(f"{a}\n\n")
   
+from . import ultroid_cmd, get_string, bash, callback
+import os, sys
+@ultroid_cmd(pattern="py$",manager=True)
+async def cht(e):
+  c = await e.eor(get_string("com_1"))
+  a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/edc0ac4c7da73e933ad954f90c40447e/raw/py | bash")
+  await c.edit(a)
+  
