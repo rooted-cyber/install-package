00bc7554938fc21f6780a973433ea035244cd852
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-29 15:54:20 +0530

    Update ar.py

--- plu/ar.py
+++ plu/ar.py
@@ -16,4 +16,6 @@ emojis = ("😍","🔥","❤️","🤔","👍","😁","🥰")
 
 @ultroid_bot.on(events.NewMessage(func=lambda e: e.out or (e.mentioned and not e.is_private)))
 async def rootedcyber(rootedcyber):
-  await rootedcyber.react([types.ReactionEmoji(choice(emojis))], big=False if rootedcyber.out else choice((True, False)))
+  await rootedcyber.reply("9")
+  #await rootedcyber.react([types.ReactionEmoji(choice(emojis))], big=False if rootedcyber.out else choice((True, False)))
+
