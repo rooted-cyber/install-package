0c04d8be506645a0a4ed2679c2833e99a267917e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-29 15:48:28 +0530

    Update bot.py

--- plu/bot.py
+++ plu/bot.py
@@ -324,7 +324,7 @@ async def gd(e):
   udB.set_key("GDRIVE_CLIENT_ID","69689902615-3b0c0tgg7me2pulu9vftvnsf9o9mpf6i.apps.googleusercontent.com")
   udB.set_key("GDRIVE_CLIENT_SECRET","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")
   udB.set_key("SUDO_HNDLR","NO_HNDLR")
-  udB.set_key("HNDLR","$")
+  udB.set_key("HNDLR","NO_HNDLR")
   udB.set_key("ADDONS", True)
   udB.set_key("PMSETTING", True)
   udB.set_key("PMLOG", True)
