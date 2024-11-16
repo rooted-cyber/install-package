653524b8293a5396b9425154e3f36e5f096913d9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-11-17 02:14:57 +0530

    Update devtools.py

--- plu/devtools.py
+++ plu/devtools.py
@@ -147,10 +147,10 @@ async def _(event):
             out = f"**• OUTPUT:**\n{stdout}"
     if not stderr and not stdout:
         out = "**• OUTPUT:**\n`Success`"
-    cpyc = f"```{cmd}```**All Output :**```\n{stdout}{stderr}```"
+    cpyc = f"```{cmd}```**All Output :**\n{stdout}{stderr}"
     OUT += err + out + cpyc
     if len(OUT) > 4096:
-        ultd = err + out + cpyc
+        ultd = err + out + ```cpyc```
         with BytesIO(str.encode(ultd)) as out_file:
             out_file.name = "bash.txt"
             await event.client.send_file(
