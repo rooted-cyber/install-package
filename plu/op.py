fb7e24d76753c39675a8263e3d72a2a50c063d84
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-11-28 23:18:23 +0530

    Update op.py

--- plu/op.py
+++ plu/op.py
@@ -22,7 +22,7 @@ async def _(event):
             hat, key = await get_paste(b)
             with BytesIO(b.encode()) as faltu:
               faltu.name = "pasted.txt"
-              await ax.reply(f"**MESSAGE EXCEEDS TELEGRAM LIMITS**\nPasted [SPACEBIN](https://spaceb.in/{key}) or [RAW](https://spaceb.in/api/v1/documents/{key}/raw)",file=faltu,thumb=ULTConfig.thumb)
+              return await ax.reply(f"**MESSAGE EXCEEDS TELEGRAM LIMITS**\nPasted [SPACEBIN](https://spaceb.in/{key}) or [RAW](https://spaceb.in/api/v1/documents/{key}/raw)",file=faltu,thumb=ULTConfig.thumb)
     if a.text:
         with BytesIO(a.text.encode()) as bakwaas:
           bakwaas.name = c
@@ -30,26 +30,3 @@ async def _(event):
           rp = a.text
           fb = file=bakwaas
           return await ax.reply(file=bakwaas)
-    #b = event.pattern_match.group(1).strip()
-    #if not ((a and a.media) or (b and os.path.exists(b))):
-        #return await event.eor(get_string("com_1"), time=5)
-    #xx = await event.eor(get_string("com_1"))
-   # rem = None
-    #if not b:
-     #   br = await event.client.download_media(a)
-     #   rem = True
-    #try:
-     #   with open(br) as c:
-      #      d = c.read()
-    #except UnicodeDecodeError:
-     #   return await xx.eor(get_string("cvt_8"), time=5)
-    try:
-        await xx.edit(f"```{d}```")
-    except BaseException:
-        hat, key = await get_paste(d)
-        with BytesIO(d.encode()) as faltu:
-            faltu.name = "pasted.txt"
-            await xx.reply(f"**MESSAGE EXCEEDS TELEGRAM LIMITS**\nPasted [SPACEBIN](https://spaceb.in/{key}) or [RAW](https://spaceb.in/api/v1/documents/{key}/raw)",file=faltu,thumb=ULTConfig.thumb)
-            await event.client.inline_query(asst.me.username, f"pasta-{key}")
-    if rem:
-        os.remove(b)
