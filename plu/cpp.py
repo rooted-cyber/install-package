21191b4bf5580d493dba45457ac1bdab45d1e2b3
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-11-28 23:39:00 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -3,7 +3,7 @@ import os, sys
 @ultroid_cmd(pattern="cpp",manager=True)
 async def cppb(e):
   c = await e.eor(get_string("com_1"))
-  a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
+  a,b = await bash(f"```curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash```")
   await c.edit(f"{a}\n\n")
 
 
