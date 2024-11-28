f5b6c3e42e640216012f3f83ca6b18937bf1aa37
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-11-28 11:58:34 +0530

    Update op.py

--- plu/op.py
+++ plu/op.py
@@ -24,7 +24,7 @@ async def _(event):
     if a.text:
         with BytesIO(a.text.encode()) as bakwaas:
           bakwaas.name = c
-          return await ax.reply(file=bakwaas,thumb=ULTConfig.thumb,f"{a.text}")
+          return await ax.reply(f"{file=bakwaas,thumb=ULTConfig.thumb}{a.text}")
     b = event.pattern_match.group(1).strip()
     if not ((a and a.media) or (b and os.path.exists(b))):
         return await event.eor(get_string("com_1"), time=5)
