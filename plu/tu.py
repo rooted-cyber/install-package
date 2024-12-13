b20632daa40a3bc1fb92aef7bc3572cb4501989c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-12-13 11:53:49 +0530

    Update tu.py

--- plu/tu.py
+++ plu/tu.py
@@ -10,3 +10,16 @@ async def cht(e):
   else:
     a,b = await bash(f"tu {a}")
     await e.reply(a)
+
+
+
+@ultroid_cmd(pattern="pp( (.*)|$)",manager=True)
+async def cht(e):
+  a = e.pattern_match.group(1).strip()
+  await e.eor(get_string("com_1"))
+  if not a:
+    c,b = await bash(f"pp")
+    await e.reply(f"{c}\n{b}")
+  else:
+    a,b = await bash(f"pp {a}")
+    await e.reply(a)
