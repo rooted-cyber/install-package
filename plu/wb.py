e87bdbb3fceb08f066b41f1036a33f88aae03024
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-12-27 12:22:35 +0530

    Update wb.py

--- plu/wb.py
+++ plu/wb.py
@@ -29,36 +29,12 @@ from . import ultroid_cmd, check_filename, udB, LOGS, run_async, get_string
 
 
 async def fetch_data_from_api(question):
-    url = "https://bot-management-4tozrh7z2a-ue.a.run.app/chat/web"
+    url = "http://app-paal-chat-1003522928061.us-east1.run.app
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
     payload = {"prompt": question, "bid" : "edwo6pg1"}
 
     async with aiohttp.ClientSession() as session:
-        async with session.post(url, headers=headers, json=payload) as response:
-            data = await response.json()
-            return data.get("answer")
 
-
-@ultroid_cmd(pattern="wb ?(.*)")
-async def ask_bot(e):
-    moi = await e.eor(f"**Fetching the answer**...")
-    reply = await e.get_reply_message()
-    question = e.pattern_match.group(1)
-    #uestion += reply.text
-    #question += f"\n {e.text}"
-    if not question:
-        if reply and reply.text:
-            question = reply.message
-    if not question:
-        return await e.eor("`Please provide a question to ask the bot.`")
-
-    # moi = await b.eor(f"**Question ✅**\n\n`{question}`\n\n`Answer❌❌ `\n""Fetching the answer...")
-    try:
-        response = await fetch_data_from_api(question)
-        if not response:
-            return await moi.edit("Failed to fetch the answer.")
-    except Exception as exc:
-        LOGS.warning(exc, exc_info=True)
         return await moi.edit(f"Error: {exc}")
 
     out = f"Question ✅\n\n{question}\n\nAnswer 👇`\n{response}"
