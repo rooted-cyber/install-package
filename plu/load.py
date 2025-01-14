b7e19db85fd447611c89a71c28ac8caa9326da8f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-01-15 03:43:25 +0530

    Update load.py

--- plu/load.py
+++ plu/load.py
@@ -11,7 +11,7 @@ async def ch(e):
     return
   b = f"{q}.py"
   d = ls("plugins")
-  s = ls("Addons")
+  s = ls("addons")
   if b in d:
     pr = await e.eor(get_string("com_1"))
     load_addons(f"plugins/{q}.py")
@@ -21,7 +21,7 @@ async def ch(e):
 
   if b in s:
     pr = await e.eor(get_string("com_1"))
-    load_addons(f"Addons/{q}.py")
+    load_addons(f"addons/{q}.py")
     await pr.edit(f"Successfully loaded **{q}** Addons plugin")
   else:
      await e.respond(f"**{q}** `Addons folder not found`")
