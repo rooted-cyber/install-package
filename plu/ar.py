4fcf8409d6167ff73ba8737dc0f4c75cc36a9c0f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-01-18 03:54:27 +0530

    Update ar.py

--- plu/ar.py
+++ plu/ar.py
@@ -13,12 +13,3 @@ async def linkrss(e):
     return LOGS.exception(ex)
   s("tof maruf")
 
-
-@ultroid_cmd(pattern="msg( (.*))")
-async def linkrhss(e):
-  ab = await event.get_reply_message()
-  ty = event.pattern_match.group(1)
-  if not ab:
-    await e.reply(f"{ty}",parse_mode="html")
-  else:
-    await e.reply(f"{ab.text}",parse_mode="html")
