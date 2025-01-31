5e2e6c2d832ba15236faf554e861e81b211d3db0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-01-31 19:03:30 +0530

    Update bot.py

--- plu/bot.py
+++ plu/bot.py
@@ -214,10 +214,10 @@ async def _(event):
     c = inline_mention(event.sender)
     command_received_time = time.time()
     end = round((time.time() - command_received_time) * 1000)
-    reply_to_id = event.reply_to_msg_id or event.id
+    #reply_to_id = event.reply_to_msg_id or event.id
     uptime = time_formatter((time.time() - start_time) * 1000)
     #await event.delete()
-    x = await event.edit(f"<pre><b>Bot start time</b> : s {end} <code>{uptime}</code>\n<b>Time</b> : {b}\n<b>Date</b> : {a}\n<b>Owner</b> : <code>{c}</code></pre>",file=udB.get_key("ALIVE_PIC"),reply_to=reply_to_id,parse_mode=parse)
+    x = await event.edit(f"<pre>{end}\nBot start time</b> : <code>{uptime}</code>\n<b>Time</b> : {b}\n<b>Date</b> : {a}\n<b>Owner</b> : <code>{c}</code></pre>",file=udB.get_key("ALIVE_PIC"),parse_mode=parse)
     await pi(event)
 @ultroid_cmd(
     pattern="rs$",
