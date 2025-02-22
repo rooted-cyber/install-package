46b47cb699782200dd6581f7e16149fb8523c749
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-02-22 11:38:55 +0530

    Update load.py

--- plu/load.py
+++ plu/load.py
@@ -6,7 +6,7 @@ from . import ultroid_cmd, get_string
 async def ch(e):
   q = e.pattern_match.group(1).strip()
   reply = await e.get_reply_message()
-  if not reply:
+  if not q:
     await e.respond("**Type plugin name or reply**")
     return
   b = f"{q}.py"
