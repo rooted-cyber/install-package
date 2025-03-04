bd5c4854b5ed4bd78818568c79504825f6985261
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-04 17:41:16 +0530

    Update wb.py

--- plu/wb.py
+++ plu/wb.py
@@ -76,7 +76,7 @@ async def ask_bot(e):
         return await moi.edit(f"Error: {exc}")
 
     try:
-      out = f"{pb}  **web**  {pb}\n~ `{question}`\n{pb}*******{pb}\n\n ~ **{response}**"
+      out = f"{pb}  **web**  {pb}\n~ `{question}`\n{pb}••••••••••••••{pb}\n\n ~ **{response}**"
       await e.edit(f"{out}",parse_mode="md")
     except:
       with BytesIO(out.encode()) as outf:
