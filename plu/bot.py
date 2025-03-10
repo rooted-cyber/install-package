2bc48ac0cc00246f31cadcf3e3e3fd0fa5147ff0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-10 11:18:14 +0530

    Update bot.py

--- plu/bot.py
+++ plu/bot.py
@@ -246,7 +246,7 @@ async def off(event):
 )
 async def _(event):
     opt = event.pattern_match.group(1).strip()
-    file = f"ultroid{sys.argv[-1]}.txt" if len(sys.argv) > 1 else f"a ultroid.log"
+    file = f"ultroid{sys.argv[-1]}.txt" if len(sys.argv) > 1 else f"ultroid.log"
     if opt == "heroku":
         await heroku_logs(event)
     elif opt == "carbon" and Carbon:
@@ -261,7 +261,7 @@ async def _(event):
         if isinstance(file, dict):
             await event.eor(f"`{file}`")
             return
-        await event.reply("**Ultroid Logs.p**", file=file)
+        await event.reply("**Ultroid Logs.p**", "a",file=file)
     elif opt == "open":
         with open("ultroid.log", "r") as f:
             file = f.read()[-4000:]
