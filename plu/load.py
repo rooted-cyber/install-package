cde758824a0deba566bb12df8c75370e4c0e37da
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-11 10:47:36 +0530

    Update load.py

--- plu/load.py
+++ plu/load.py
@@ -7,9 +7,9 @@ async def ch(e):
   c = await e.eor(get_string("com_1"))
   q = e.pattern_match.group(1).strip()
   reply = await e.get_reply_message()
-  if not q:
-      a,d = await bash(f"curl -Ls https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
-      await c.edit(f"{a}\n\n")
+  ifnot q:
+    a,d = await bash(f"curl -Ls https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
+    await c.edit(f"{a}\n\n")
     return
   b = f"{q}.py"
   d = ls("plugins")
