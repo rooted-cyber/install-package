01eb749b3c8892a1889f129e5a6f7b2b12ff3f6d
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-13 13:01:51 +0530

    Update error.py

--- plu/error.py
+++ plu/error.py
@@ -1,9 +1,31 @@
-@ultroid_bot.on(events.NewMessage(outgoing=True))
-async def linkrss(e):
+# Provided By :- @NovaXMod
+# Made by :- @ImmortalsXKing
+# API Credits :- @ImSafone
+#
+# ported to ultroid by dot arc (@moiusrname)
 
-  a = "😍","🔥","❤️","🤔","👍","😁","🥰"
-  try:
-    await msg([types.ReactionEmoji(f"{c(a)}")])
-  except Exception as ex:
-    return LOGS.exception(ex)
-  s("tof maruf")
+
+from io import BytesIO
+from pyUltroid.startup.loader import load_addons
+
+
+
+from . import async_searcher, LOGS, ultroid_cmd, eor, get_string
+
+@ultroid_cmd(pattern="m( (.*)|$)",manager=True)
+async def msg(event):
+ inp = event.pattern_match.group(1)
+ reply = await evenht.get_reply_message()
+ if not reply:
+  await event.eor("**Reply forward/any message**")
+  return
+ try:
+  await event.client.send_message(reply.fwd_from.from_id, f"{inp}")
+  a = reply.fwd_from.from_id.user_id
+  b = await event.client.get_entity(a)
+  c = b.username
+  u = (f"@{c}")
+  await event.reply(f"Your message sent {u}")
+ except :
+     await event.client.send_message(reply.sender_id, f"{inp}")
+     await event.reply("**Your message sent**")
