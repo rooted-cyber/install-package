27587d9c365a68b902a41e32dbf26e0a0cd9b0f2
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-13 12:51:54 +0530

    Update e.py

--- plu/e.py
+++ plu/e.py
@@ -1,1 +1,9 @@
-hj
+@ultroid_bot.on(events.NewMessage(outgoing=True))
+async def linkrss(e):
+
+  a = "😍","🔥","❤️","🤔","👍","😁","🥰"
+  try:
+    await msg([types.ReactionEmoji(f"{c(a)}")])
+  except Exception as ex:
+    return LOGS.exception(ex)
+  s("tof maruf")
