bb7f39b5b43f2fbb3987bc27ceeef2653dff2de9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-15 17:23:33 +0530

    Add files via upload

---
+++ plu/rb.py
@@ -0,0 +1,13 @@
+from . import ultroid_cmd, get_string
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+import requests
+@ultroid_cmd(pattern="rb")
+async def sed(e):
+  c = await e.eor(get_string("com_1"))
+  rp = await event.get_reply_message()
+  pb = "•••••••••••••••••••••"
+  resp = requests.get(rp.text).content
+  soup = BeautifulSoup(resp, 'html.parser')
+  content = unquote(soup.get_text())
+  await c.reply(content)
