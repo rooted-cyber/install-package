731e2662ff461a2a077ec3842873736b36c2a5db
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-15 19:48:45 +0530

    Add files via upload

--- plu/rb.py
+++ plu/rb.py
@@ -9,12 +9,18 @@ e from . import ultroid_cmd, get_string
 from . import ultroid_cmd, get_string
 from urllib.parse import unquote
 from bs4 import BeautifulSoup
+from io import BytesIO aas op
 import requests
 rp = await event.get_reply_message()
 resp = requests.get(rp.text).content
 soup = BeautifulSoup(resp, 'html.parser')
 content = unquote(soup.get_text())
-await e.reply(content)
+try:
+  a = await e.reply(content)
+except:
+  with op(a.encode()) as op:
+    op.name = "hi.txt"
+    await e.reply(file="op")
 """
  
   await e.reply(f"<pre>{a}</pre>",parse_mode="html")
