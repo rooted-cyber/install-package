2d58723a3d34739ef82ff43ae0d99842e62ccc99
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-05-30 14:05:36 +0530

    Update bot.py

--- plu/bot.py
+++ plu/bot.py
@@ -245,10 +245,10 @@ async def off(event):
 )
 async def _(event):
     opt = event.pattern_match.group(1).strip()
-    file = f"ultroid{sys.argv[-1]}.txt" if len(sys.argv) > 1 else f"ultroid.log"
+    file = f"ultroid{sys.argv[-1]}.txt" if len(sys.argv) > 1 else f"ultroid.txt"
     from pathlib import Path
     ps , af = await get_paste(Path("ultroid.log").read_text())
-    await event.eor(f"**Ultroid Logs. [pasted here](https://spaceb.in/{af}**",file=file,thumb=ULTConfig.thumb,parse_mode="md")
+    await event.eor(f"**Ultroid Logs**. [pasted here](https://spaceb.in/{af})",file=file,thumb=ULTConfig.thumb,parse_mode="md")
     with open(file, "r") as f:
         code = f.read()[-2500:]
         file = await Carbon(
