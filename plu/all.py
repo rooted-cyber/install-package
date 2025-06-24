f799cb18b03eaa68c9b14dc26f38baef46d5a72f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-06-24 19:53:50 +0530

    Update all.py

--- plu/all.py
+++ plu/all.py
@@ -4,9 +4,11 @@ async def count_messages(e):
     n = ""
     async for m in e.client.iter_participants(e.chat_id):
         b = m.id
+        c = f"[**{m.first_name}**](tg://user?id={b}"
+        d = f"**{m.first_name}**"
         try:
             a = await e.client.get_messages(e.chat_id, limit=0, from_user=b)
-            n += f"*{m.first_name}*: {a.total} msgs\n"
+            n += f"*{m.first_name}*{c} : {a.total} msgs\n"
         except Exception as ex:
             print(f"Error fetching messages for {m.first_name}: {ex}")
     await e.eor(n or "No data found.")
