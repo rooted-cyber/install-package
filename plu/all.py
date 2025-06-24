431d3843e7fca11d873677ebcc561b706c5ad2fd
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-06-24 20:55:16 +0530

    Update all.py

--- plu/all.py
+++ plu/all.py
@@ -15,3 +15,29 @@ async def count_messages(e):
             print(f"Error fetching messages for {m.first_name}: {ex}")
     await e.eor(n or "No data found.",parse_mode="md")
     
+
+from . import ultroid_cmd
+
+@ultroid_cmd(pattern="total$")
+async def count_messages(e):
+    await e.eor("Processing...")
+    n = ""
+    total_msgs = 0
+    user_count = 0
+
+    async for m in e.client.iter_participants(e.chat_id):
+        b = m.id
+        name = f"[*{m.first_name}*](tg://user?id={b})"
+        try:
+            a = await e.client.get_messages(e.chat_id, limit=0, from_user=b)
+            n += f"{name} : {a.total} msgs\n"
+            total_msgs += a.total
+            user_count += 1
+        except Exception as ex:
+            print(f"Error fetching messages for {m.first_name}: {ex}")
+
+    if not n:
+        await e.eor("No data found.")
+    else:
+        msg = f"*Total Msgs:* `{total_msgs}`\n*Total Users:* `{user_count}`\nn"
+        await e.eor(msg, parse_mode="md")
