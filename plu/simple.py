2ef06ae0302ff9ae7350b13af104919d9e432ad4
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-06-25 17:20:18 +0530

    Update simple.py

--- plu/simple.py
+++ plu/simple.py
@@ -1,6 +1,6 @@
 from . import ultroid_cmd, get_string, bash
-#0Best plugin
-😂😂😂🤣
+#Best plugin
+#😂😂😂🤣
 @ultroid_cmd(pattern="hi",manager=True)
 async def acht(e):
   x = await e.eor(get_string("com_1"))
