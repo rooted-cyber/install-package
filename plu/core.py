455d8dc44aeaf1904a29d08ff57f79eb12e6bfa3
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-07-04 03:51:20 +0530

    Update core.py

--- plu/core.py
+++ plu/core.py
@@ -18,6 +18,7 @@ from . import LOGS, async_searcher, eod, get_string, safeinstall, ultroid_cmd, u
 async def speed(e):
   a = await e.edit("`Checking your ultroid userbot speed....`")
   await sp(e)
+  await a.delete()
 
 @ultroid_cmd(pattern="ins", fullsudo=True)
 async def install(event):
