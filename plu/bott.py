4ad66acaa578ac59fe0ba399173b93f2b0a3eb15
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-07-29 11:34:00 +0530

    Update bott.py

--- plu/bott.py
+++ plu/bott.py
@@ -158,7 +158,7 @@ async def token_by_username(e):
     await e.client.send_message(bf, "/token")
     await e.client.send_message(bf, f"{username}")
     rp = (await ultroid_bot.get_messages(bf, limit=1))[0].text
-    if not rp.startswith("You"):
+    if rp.startswith("You"):
       token = rp.split("`")[1]
-      await e.eor(f"""Bot : {username}
+      await e.reply(f"""Bot : {username}
         token : {token}""")
