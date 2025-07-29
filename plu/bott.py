c34ad24af94831ee6ebee39849005802c6802bde
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-07-29 21:02:38 +0530

    Update bott.py

--- plu/bott.py
+++ plu/bott.py
@@ -165,7 +165,7 @@ async def token_by_username(e):
           token = rp.split("`")[1]
           await e.client.send_message(e.chat_id,f"""Bot : **{username}**
           
-token : **{token}**""")
+token : `{token}`""")
       except Exception as e:
           await e.eor(e)
    
