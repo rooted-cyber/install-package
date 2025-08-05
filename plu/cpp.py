e9a901566c905632c6ce6812ca97193ed28fe3cb
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-08-05 17:12:53 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -1,4 +1,4 @@
-from . import ultroid_cmd, get_string, bash, call_back
+from . import udB, ultroid_cmd, get_string, bash, call_back
 import os, sys
 @ultroid_cmd(pattern="cpp",manager=True)
 async def cppb(ult):
