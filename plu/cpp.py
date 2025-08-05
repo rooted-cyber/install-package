721c92617b7e2a00c132e792ec0032b8232138a6
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-08-05 14:10:21 +0530

    Update cpp.py

--- plu/cpp.py
+++ plu/cpp.py
@@ -2,7 +2,7 @@ from . import ultroid_cmd, get_string, bash, callback
 import os, sys
 @ultroid_cmd(pattern="cpp",manager=True)
 async def cppb(ult):
-  c = await e.eor(get_string("com_1"))
+  c = await ult.eor(get_string("com_1"))
   a,d = await bash(f"curl -Ls https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
   await c.edit(f"{a}\n\n")
   ok = await ult.eor(get_string("bot_5"))
